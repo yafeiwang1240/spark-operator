879e6b05abe13d98ea064998dbe5c0af621b13fb
wangyafei <364968487@qq.com>
2021-01-02 16:17:28 +0800

    repartition

--- src/main/java/com/github/yafeiwang1240/Job.java
+++ src/main/java/com/github/yafeiwang1240/Job.java
@@ -1,10 +1,11 @@
 package com.github.yafeiwang1240;
 
-import com.github.yafeiwang1240.sparkoperator.action.Broadcast;
+import com.github.yafeiwang1240.sparkoperator.transformation.keyvalue.RePartition;
 
 public class Job {
     public static void main(String[] args) {
-        Function function = new Broadcast();
+        Function function = new RePartition();
+
         function.function();
     }
 }
---
+++ src/main/java/com/github/yafeiwang1240/sparkoperator/transformation/keyvalue/RePartition.java
@@ -0,0 +1,49 @@
+package com.github.yafeiwang1240.sparkoperator.transformation.keyvalue;
+
+import com.github.yafeiwang1240.Function;
+import org.apache.spark.HashPartitioner;
+import org.apache.spark.Partitioner;
+import org.apache.spark.RangePartitioner;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.ForeachFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 说明：spark算子，repartition
+ * 特点：单个RDD聚集
+ * 执行：repartition算子在executor端执行分区
+ * sql: 开窗
+ * @author wangyafei
+ */
+public class RePartition implements Function {
+
+    @Override
+    public void function() {
+        SparkSession session = null;
+        try {
+            session = SparkSession.builder().appName("rePartition")
+                    .enableHiveSupport().getOrCreate();
+            org.apache.spark.sql.Dataset<Row> ds = session.sql("select c_name as name, c_workyear as value from user_c limit 100");
+            ds.repartition(2).foreach(new ForeachFunction<Row>() {
+                @Override
+                public void call(Row row) throws Exception {
+                    System.out.println(row);
+                }
+            });
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
