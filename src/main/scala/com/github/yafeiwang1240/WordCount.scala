663bc3cfcd68f6616020363bb23b92c58899b79c
wangyafei <364968487@qq.com>
2021-08-19 15:45:44 +0800

    scala wordcount

---
+++ src/main/scala/com/github/yafeiwang1240/WordCount.scala
@@ -0,0 +1,16 @@
+package com.github.yafeiwang1240
+
+import org.apache.spark.sql.SparkSession
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("WordCount").getOrCreate()
+    val rdd = spark.sparkContext.textFile("/tmp/wordcount/word.txt")
+    val pair = rdd.map(x => (x, 1))
+    val reduce = pair.reduceByKey((x, y) => x + y)
+    reduce.collect().foreach((x) => println(x._1 + ": " + x._2))
+    spark.close()
+  }
+
+}
