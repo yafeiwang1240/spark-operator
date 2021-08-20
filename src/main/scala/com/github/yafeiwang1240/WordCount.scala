4bc89afec76af17df3bbadb16773da5038e2ed44
wangyafei <364968487@qq.com>
2021-08-20 20:42:29 +0800

    pom

--- src/main/scala/com/github/yafeiwang1240/WordCount.scala
+++ src/main/scala/com/github/yafeiwang1240/WordCount.scala
@@ -6,7 +6,7 @@ object WordCount {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("WordCount").getOrCreate()
-    val rdd = spark.sparkContext.textFile("/tmp/wordcount/word.txt")
+    val rdd = spark.sparkContext.objectFile("/tmp/wordcount/")
     val pair = rdd.map(x => (x, 1))
     val reduce = pair.reduceByKey((x, y) => x + y)
     reduce.collect().foreach((x) => println(x._1 + ": " + x._2))
